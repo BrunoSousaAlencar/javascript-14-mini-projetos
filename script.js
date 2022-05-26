a64284781d38acb62193e392411cc4be6e6e940f
Bruno e Sousa Alencar <brunoeesousanoleto2017@gmail.com>
2022-05-26 16:02:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = 'Rafael'; //string literal
+let idade = 25; //number literal
+let estaAprovado = true; //boolean
+let sobrenome = undefined; //Undefined
+let corSelecionado = null; //Redefinir um valor
+
+let pessoa = {
+    nome: 'Naruto',
+    idade: 28,
+    estaAprovado: true,
+    sobrenome:'de sousa',
+}
+
+console.log(pessoa);
