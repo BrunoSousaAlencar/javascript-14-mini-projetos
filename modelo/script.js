fd7952ad569292fb3f57e30b2fa78bea321b17d5
Bruno e Sousa Alencar <brunoeesousanoleto2017@gmail.com>
2022-05-27 19:04:17 -0300

    operadores

--- modelo/script.js
+++ modelo/script.js
@@ -1,26 +1,4 @@
-/*
-//Operadores 
-//Operadores Aritiméticos
-
-let salario = 5
-
-//+ , -, *, /, **
-
-console.log(salario + salario)
-console.log(salario - salario)
-console.log(salario * salario)
-console.log(salario / salario)
-console.log(salario ** salario)
-*/
-
-// ++ --
-
-let idade = 18
-
-console.log(++idade)
-console.log(--idade)
-
-
+window.alert('Olá, Mundo!')
 
 
 
---
+++ operadores/lógicos/script.js
@@ -0,0 +1,31 @@
+// Operadores Lôgicos e (&&)
+//Retorna true se os dois Operadores forem true 
+ 
+//console.log(true && false)
+//console.log(false && false)
+
+/*
+let maiorDeIdade = false
+let possuicarteiraDeTabalho = true
+let podeAplicar = maiorDeIdade && possuicarteiraDeTabalho
+
+console.log(podeAplicar)
+*/
+
+// Operadores Lôgicos e (||)
+//retorne true se um dos operados
+
+let maiorDeIdade = false
+let possuicarteiraDeTabalho = false
+let podeAplicar = maiorDeIdade || possuicarteiraDeTabalho
+
+console.log('Pode aplicar', podeAplicar)
+
+// Operador NOT (!)
+let candidatoRecusado = !podeAplicar
+
+console.log('Candidato recusado', candidatoRecusado)
+
+
+
+
